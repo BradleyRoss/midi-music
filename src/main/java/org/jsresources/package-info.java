727b70a525aadcfd09ebd21ee8e431b2de741c95
Bradley Ross <bradleyross@me.com>
2017-06-13 19:46:27 -0400

    Adding jsresources routine

---
+++ src/main/java/org/jsresources/package-info.java
@@ -0,0 +1,8 @@
+package org.jsresources;
+/**
+ *  These are samples from Java Sound Resources.
+ *  
+ *  <p>I had to remove CddaPlayer and CddaRipper because they 
+ *     appear to use classes that are not in the repository for
+ *     Tritonus.</p>
+ */
