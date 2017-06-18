ee86408bbd31cae7f7e9d6224bb4e436d440e3c4
Bradley Ross <bradleyross@me.com>
2017-06-18 12:49:06 -0400

    Removing deleted files from repository

--- src/main/java/bradleyross/music/external/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * These are classes obtained from external sources.
- * 
- *
- */
-package bradleyross.music.external;
--- src/main/java/bradleyross/music/playpen/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This package is for testing concepts related to the javax.sound packages.
- * 
- * <p>It appears that there are problems running these classes from inside Eclipse although
- *    they work when executed from the command line.</p>
- * @author Bradley Ross
- *
- */
-package bradleyross.music.playpen;
